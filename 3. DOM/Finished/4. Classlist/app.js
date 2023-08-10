4380a1f6c610d8bbaf87a5b1afb1bc38b24abbf6
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:31:34 -0700

    Classlist

---
+++ 3. DOM/Finished/4. Classlist/app.js
@@ -0,0 +1,15 @@
+// classlist
+// add()
+// remove()
+// toggle()
+
+const h1 = document.querySelector("h1");
+// BROWSER CONSOLE
+console.log(h1.classList);
+// console.log((h1.style.color = "skyblue"));
+// console.log((h1.style.fontWeight = "normal"));
+
+// In EDITOR
+h1.classList.add("styles");
+h1.classList.remove("styles");
+h1.classList.toggle("styles");
