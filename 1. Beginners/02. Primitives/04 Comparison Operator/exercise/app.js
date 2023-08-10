5dd5edecee33bdef63520271c10027677a10e5a5
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:18:47 -0700

    Comparison Operator

---
+++ 1. Beginners/02. Primitives/04 Comparison Operator/exercise/app.js
@@ -0,0 +1,23 @@
+// 1. Create variable name (firstFavNumb) & store your favorite number.
+// 2. Create variable name (secondFavNumb) & store second fav Number.
+// 3. Check (firstFavNumb is greater then secondFavNumb)
+// 4. Check (firstFavNumb is less then secondFavNumb)
+// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
+// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
+// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
+// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
+// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
+
+let firstFavNumb = 21;
+let secondFavNumb = 10;
+
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
---
+++ 1. Beginners/02. Primitives/04 Comparison Operator/live coding/app.js
@@ -0,0 +1,23 @@
+// ***********************************
+// RELATIONAL OPERATORS
+// > Greater Then
+// < Less Then
+// >= Greater Then or equal to
+// <= Less Then or equal to
+
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+// ***********************************
+// EQUALITY OPERATORS
+// === strict equality (Type + Value)
+// !== strict non-equality (Type + Value)
+// == Lose equality (values)
+// != Lose equality (values)
+
+console.log(10 === 10);
+console.log(10 !== 10);
+console.log(10 == 10);
+console.log(10 != 10);
