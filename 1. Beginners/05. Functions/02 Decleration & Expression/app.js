8abc6c355ec1475633fd20592d9376b6ce519ecc
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:22:58 -0700

    Function Decleration & Expression

---
+++ 1. Beginners/05. Functions/02 Decleration & Expression/app.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function sayHello(username) {
+  console.log(`Hello ${username}`);
+}
+
+sayHello("HuXn");
+
+// Function Expressions
+const greetings = function (user) {
+  console.log(`Hello ${user}`);
+};
+
+greetings("Doe");
+
+// -------------------------
+console.log(x); // ERROR
+let x = 10;
