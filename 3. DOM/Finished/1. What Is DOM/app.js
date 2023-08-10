f2961a6a549f305e892e07eb446c3daeb9fba5c6
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:30:00 -0700

    What is DOM

---
+++ 3. DOM/Finished/1. What Is DOM/app.js
@@ -0,0 +1,8 @@
+// console.log(document);
+
+console.log(document);
+console.log(document.head);
+console.log(document.title);
+console.log(document.body);
+console.log(document.documentURI);
+console.log(document);
