08bf05520f5c4594d736357275621099d448ff16
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:17:29 -0700

    Basics (console)

---
+++ 1. Beginners/01. Basics/console.js
@@ -0,0 +1,23 @@
+// ---------- CONSOLE ----------
+console.log("Hello World");
+// log
+// clg
+// console.log(console); // console object
+
+console.error("This is an ERROR");
+console.warn("Warning");
+console.table({ name: "HuXn", age: 20, location: ["USA"] });
+
+// ---------- COMMENTS ----------
+
+// This is one line comment
+/*
+    This is 
+    multi-line 
+        comment
+*/
+/** Unordered Comment
+ * bla
+ * bla
+ * bla
+ */
