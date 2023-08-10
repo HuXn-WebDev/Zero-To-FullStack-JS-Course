1b83b437522bc276a87c96218f513f0b37c539bf
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:18:18 -0700

    Numbers

---
+++ 1. Beginners/02. Primitives/02 Numbers/exercise/app.js
@@ -0,0 +1,18 @@
+// 1. Create Variable name (firstFavNum) & store your favorite number.
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+// 3. Add (firstFavNum & secondFavNumber).
+// 4. Subtract (firstFavNum & secondFavNumber).
+// 5. Multiply (firstFavNum & secondFavNumber).
+// 6. Divided (firstFavNum & secondFavNumber).
+// 7. Check (firstFavNum Mod secondFavNumber)
+// 8. Check the power of (firstFavNum)
+
+let firstFavNum = 21;
+let secondFavNum = 7;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
---
+++ 1. Beginners/02. Primitives/02 Numbers/live code/app.js
@@ -0,0 +1,19 @@
+// JavaScript has one number type
+// Positive Number
+// Negative Number
+// Whole Number (integers)
+// Decimal Numbers
+
+const randomNumber = 10200;
+const favNumber = 21;
+let num = 2;
+
+// Basic Math
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+console.log(num++);
+console.log(num--);
