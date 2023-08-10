b4815a4922d67f88266dbfc6b28aa69416778780
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:20:45 -0700

    while loop

---
+++ 1. Beginners/03. Control Flow/04 While Loop/exercise/app.js
@@ -0,0 +1,9 @@
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let i = 10;
+
+while (i <= 100) {
+  console.log("HuXn WebDev");
+  i++;
+}
---
+++ 1. Beginners/03. Control Flow/04 While Loop/live coding/app.js
@@ -0,0 +1,12 @@
+// while (condition) {
+//   // code block to be executed
+// }
+
+// ----------------
+let i = 1;
+
+while (i <= 5) {
+  console.log("Hello World", i);
+  i++;
+}
+// ----------------
