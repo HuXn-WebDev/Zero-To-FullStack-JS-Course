068e696aa6247d4b96e0b80224b3beaabc7d2a5b
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:50:12 -0700

    OS Module

---
+++ 6. NODE/8. OS Module/index.js
@@ -0,0 +1,10 @@
+import os from "os";
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.networkInterfaces());
+console.log(os.freemem());
+console.log(os.totalmem());
