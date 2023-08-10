6fc2d08e97f15d18a59e44cd06ed7d552986084b
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:19:20 -0700

    Type Converstion

---
+++ 1. Beginners/02. Primitives/06. Type Converstion/index.js
@@ -0,0 +1,16 @@
+let amount = 100;
+let money = "100";
+let floatValue = "99.5";
+
+// - Convert string to number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount)
+
+//  - Convert number to string
+// money = money.toString();
+// money = String(money);
+
+// - Change string to decimal
+floatValue = parseFloat(floatValue);
+console.log(floatValue);
