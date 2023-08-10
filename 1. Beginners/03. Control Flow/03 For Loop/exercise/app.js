bd504d2635b929ab4cf5b2dae9653917b90b5757
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:20:30 -0700

    for loop

---
+++ 1. Beginners/03. Control Flow/03 For Loop/exercise/app.js
@@ -0,0 +1,5 @@
+// Print your name & numbers from 0 through 1000
+
+for (let i = 0; i <= 1000; i++) {
+  console.log("HuXn WebDev", i);
+}
---
+++ 1. Beginners/03. Control Flow/03 For Loop/live coding/app.js
@@ -0,0 +1,10 @@
+// for (initialExpression; condition; incrementExpression) {}
+
+for (let i = 0; i <= 5; i++) {
+  console.log("Hello World", i);
+}
+
+// EXERCISE
+// ... (let ; = ; < ; ) {
+//   console.log(i);
+// }
