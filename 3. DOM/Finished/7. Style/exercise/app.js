564dc19ea4f3bcde735dfecf07d3ad9d0fd32247
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:32:09 -0700

    Style

---
+++ 3. DOM/Finished/7. Style/exercise/app.js
@@ -0,0 +1,20 @@
+// Selecting Elements
+const container = document.querySelector(".container");
+const h1 = document.querySelector(".main-headings");
+const p = document.querySelector(".sub-headings");
+const btn = document.querySelector(".btn");
+
+// 1. Proivde "400px" of height ot container element
+// 2. Change the background color of container to "teal"
+// 3. Change the color of h1 to "skyblue"
+// 4. Change the fontFamily of paragraph to "sans-serif"
+// 5. Change the color of paragraph to "white"
+// 6. Change the color of btn to pink
+
+// Adding Styles To Our Elements
+container.style.height = "400px";
+container.style.background = "teal";
+h1.style.color = "skyblue";
+p.style.fontFamily = "sans-serif";
+p.style.color = "white";
+btn.style.color = "pink";
---
+++ 3. DOM/Finished/7. Style/live coding/app.js
@@ -0,0 +1,3 @@
+const h1 = document.querySelector("h1");
+console.log(h1.style); // GLOBAL CSS STYLES
+h1.style.color = "skyblue";
