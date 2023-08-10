e0c6684fe180a237ff29d080578800807194f2ca
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:48:51 -0700

    REPL

---
+++ 6. NODE/2. REPL/index.js
@@ -0,0 +1,14 @@
+// Read-Eval-Print Loop
+
+// The REPL module provides a implementation that is avaliable both
+// as a standalone program or includible in other applications.
+
+// Basically it's a place where you can write/run JS code.
+
+// .break (sometimes you get stuck, this gets you out)
+// .clear (Alias for .break)
+// .editor (Enter editor mode (Ctrl+D) to finish, (Ctrl+C) to cancel)
+// .exit (Exit the REPL)
+// .help (Print this help message)
+// .load (Loads JS from a file into the REPL session e.g .load .filepath)
+// .save (Save all evaluated commands in this REPL session to a file e.g .save /filepath)
