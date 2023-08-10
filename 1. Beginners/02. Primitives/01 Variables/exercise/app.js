02177eb9a74f4287c20d3fed5101e663e31459a7
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:18:05 -0700

    Variables

---
+++ 1. Beginners/02. Primitives/01 Variables/exercise/app.js
@@ -0,0 +1,15 @@
+// 1. Create variable name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+
+let name = "HuXn WebDev";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
+let twitterHandle = "@huxnwebdev";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
---
+++ 1. Beginners/02. Primitives/01 Variables/live coding/app.js
@@ -0,0 +1,7 @@
+// (reserved keyword): (variable name): (type of value)
+
+let variable; // undefined
+let name = "HuXn WebDev";
+console.log(name);
+const channelName = "WebDev Academy";
+console.log(channelName);
