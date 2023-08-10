4bcf8693a18e0e46b3ea1a24c593c1f7be4b281d
HuXn <webdevacademy6@gmail.com>
2023-08-10 12:21:00 -0700

    do-while loop

---
+++ 1. Beginners/03. Control Flow/05 Do-While Loop/exercise/app.js
@@ -0,0 +1,9 @@
+// Print "Your Name" 400 times to the console using do-while-loop.
+// Iteration should start from 20.
+
+let i = 20;
+
+do {
+  console.log("HuXn WebDev");
+  i++;
+} while (i <= 400);
---
+++ 1. Beginners/03. Control Flow/05 Do-While Loop/live coding/app.js
@@ -0,0 +1,6 @@
+let i = 1;
+
+do {
+  console.log("Hello World", i);
+  i++;
+} while (i <= 5);
