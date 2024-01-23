413082fe0b7fd8cf072f0a3a356d585a0a541dd8
HuXn WebDev <webdevacademy6@gmail.com>
2024-01-23 04:45:28 -0800

    Small bug fixed

--- 12. MERN E-Commerce Store/frontend/src/pages/Products/ProductDetails.jsx
+++ 12. MERN E-Commerce Store/frontend/src/pages/Products/ProductDetails.jsx
@@ -19,7 +19,7 @@ import moment from "moment";
 import HeartIcon from "./HeartIcon";
 import Ratings from "./Ratings";
 import ProductTabs from "./ProductTabs";
-// import { addToCart } from "../../redux/features/cart/cartSlice";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 
 const ProductDetails = () => {
   const { id: productId } = useParams();
@@ -58,10 +58,10 @@ const ProductDetails = () => {
     }
   };
 
-  // const addToCartHandler = () => {
-  //   dispatch(addToCart({ ...product, qty }));
-  //   navigate("/cart");
-  // };
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
+  };
 
   return (
     <>
@@ -157,7 +157,7 @@ const ProductDetails = () => {
 
               <div className="btn-container">
                 <button
-                  // onClick={addToCartHandler}
+                  onClick={addToCartHandler}
                   disabled={product.countInStock === 0}
                   className="bg-pink-600 text-white py-2 px-4 rounded-lg mt-4 md:mt-0"
                 >
